b125ce26207eef639409985bad91128fe347b00c
notABluesSinger <rob@whatagoodbot.com>
2022-11-25 07:15:29 +0000

    health check back to simple

--- healthCheck.js
+++ healthCheck.js
@@ -1,6 +1,6 @@
 // TODO Make this actually check that the service is working
 // EXAMPLE = https://patrickleet.medium.com/effective-docker-healthchecks-for-node-js-b11577c3e595
 
-import { serverStatus } from './src'
+// import { serverStatus } from './src'
 
-process.exit(serverStatus)
+process.exit(0)
